7c12fcb24c1d4abefae1d2f860744c6fa52d9bba
jhs512 <jangka512@gmail.com>
2025-08-12 10:11:21 +0900

    dependencies update, spring boot 3.5.4, kotlin jvm

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.5.3"
+    id("org.springframework.boot") version "3.5.4"
     id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "1.9.25"
 }
 
 group = "com"
